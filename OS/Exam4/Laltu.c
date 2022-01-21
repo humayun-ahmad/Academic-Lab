88083c6351946cfcf29ae4fde0774d23493d29b5
humayun-ahmad <humayunraj789@gmail.com>
2022-01-21 23:55:38 +0600

    Exam4

---
+++ OS/Exam4/Laltu.c
@@ -0,0 +1,62 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
+#include <sched.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <string.h>
+#include <sys/shm.h>
+
+int main(){
+	pid_t child_a, child_b,child_c,pid, ppid,kpid,mpid;
+	int cpu;
+	
+	key_t shmKey;
+	int shmID;
+	char *str;
+	
+	shmKey = ftok("testfile", 'o');
+	//printf("SHM_key: %d\n", shmKey);
+	
+	
+	shmID = shmget(shmKey, 100, IPC_CREAT | 0666);
+	str = (char *) shmat(shmID, NULL, 0);
+	
+	child_a = fork();
+	int a = 5,b = 5;
+	
+	char ch1[200] = "10";
+	char ch2[200] = "20";
+	
+	if (child_a == 0) {
+	    /* Child a code */
+	    //printf("child_a\n");
+	    execlp("./Ittu", "./Ittu", ch1, ch2, NULL);
+	    printf("Result : %s\n", str);
+	} else {
+	    child_b = fork();
+
+	    if (child_b == 0) {
+		/* Child b code */
+		//printf("child_b\n");
+		execlp("./Bittu", "./Bittu", ch1, ch2, NULL);
+	    } else {
+		/* child c Code */
+		child_c = fork();
+		
+		if(child_c == 0){
+			//printf("child_c\n");
+			execlp("./Mittu", "./Mittu", ch1, ch2, NULL);
+		}else{
+			/* parent */
+			printf("parent\n");
+		}
+		
+	    }
+	}
+	return 0;
+}
---
+++ OS/Exam4/Mittu.c
@@ -0,0 +1,56 @@
+#define _GNU_SOURCE
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
+#include <sched.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <string.h>
+#include <sys/shm.h>
+
+int main(int argn,char *argv[]){
+	int a,b;
+	//a = argv[0];
+	//b = argv[1];
+	//printf("Hello :)\n");
+	//printf("%s %s\n", argv[1], argv[2]);
+	
+	int x = atoi(argv[1]);
+	int y = atoi(argv[2]);
+	printf("Interger value of x : %d\nInteger value of y : %d\n", x,y);
+	
+	int result = x * y;
+	printf("Result : %d\n", result);
+	
+	//printf("Hello\n");
+	//printf("value of a : %d\nvalue of b: %d\n", a, b);
+	//printf("Result : %d\n", a+b);
+	
+	key_t shmKey;
+	int shmID;
+	char *str;
+	
+	shmKey = ftok("testfile", 'o');
+	//printf("SHM_key: %d\n", shmKey);
+	
+	
+	
+	shmID = shmget(shmKey, 100, IPC_CREAT | 0666);
+	str = (char *) shmat(shmID, NULL, 0);
+	
+	//covert integer to string
+	
+	//char *st = result + '0';
+	
+	sprintf( str, "%d", result);
+	
+	//itoa(result,str,10);
+	
+	//strcpy(str, st);
+	
+	return 0;
+}
